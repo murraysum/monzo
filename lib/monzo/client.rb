ebacfeca604fc2911641f4392b387e493b7e3edd
Murray Summers <murray.sum@gmail.com>
2017-05-07 09:08:41 +0100

    Add support for creating feed items.

--- lib/monzo/client.rb
+++ lib/monzo/client.rb
@@ -24,6 +24,22 @@ module Monzo
       response
     end
 
+    def post(path, data, options = {})
+      uri = build_uri(path, options = {})
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request["Authorization"] = "Bearer #{access_token}"
+
+      request.set_form_data(data)
+
+      response = http.request(request)
+      puts response.body
+      response
+    end
+
     def build_uri(path, options)
       uri = URI.join(host, path)
       uri.query = build_query(options)
--- lib/monzo/feed_item.rb
+++ lib/monzo/feed_item.rb
@@ -1,8 +1,30 @@
 module Monzo
   class FeedItem
 
+    attr_reader :account_id, :type, :params, :url
+
     def initialize(params)
+      @account_id = params[:account_id]
+      @type = params[:type]
+      @params = params[:params]
+      @url = params[:url]
     end
 
+    def self.create(account_id, type, params, url = nil)
+      client = Monzo.client
+
+      data = {
+        "account_id" => account_id,
+        "type" => type,
+        "params[title]" => params[:title],
+        "params[image_url]" => params[:image_url],
+        "params[background_color]" => params[:background_color],
+        "params[body_color]" => params[:body_color],
+        "params[title_color]" => params[:title_color],
+        "params[body]" => params[:body],
+        "url" => url
+      }
+      client.post("/feed", data, {})
+    end
   end
 end
