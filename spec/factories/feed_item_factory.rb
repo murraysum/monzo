755084b71d5f6445cafea3b2a3549d3e4eaece08
Murray Summers <murray.sum@gmail.com>
2017-05-07 21:21:20 +0100

    Add specs for initializing for feed item.

---
+++ spec/factories/feed_item_factory.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :feed_item, :class => Monzo::FeedItem do
+    account_id "acc_00009237aqC8c5umZmrRdh"
+    type "basic"
+    params ({
+      "title" => "My custom item",
+      "image_url" => "http://www.example.com/image.png",
+      "background_color" => "#FCF1EE",
+      "body_color" => "#FCF1E1",
+      "title_color" => "#333",
+      "body" => "Some body text to display"
+    })
+    url "https://www.example.com/a_page_to_open_on_tap.html"
+
+    initialize_with do
+      new(attributes)
+    end
+  end
+end
--- spec/feed_item_spec.rb
+++ spec/feed_item_spec.rb
@@ -1,7 +1,38 @@
 require "spec_helper"
 
 describe Monzo::FeedItem do
-  it "has specs" do
-    skip "implement"
+  context "initializing an feed item" do
+    before :each do
+      @feed_item = FactoryGirl.build(:feed_item)
+    end
+
+    it "should have a account id" do
+      expect(@feed_item.account_id).to eql("acc_00009237aqC8c5umZmrRdh")
+    end
+
+    it "should have a type" do
+      expect(@feed_item.type).to eql("basic")
+    end
+
+    it "should have params" do
+      expect(@feed_item.params).to eql({
+        "title" => "My custom item",
+        "image_url" => "http://www.example.com/image.png",
+        "background_color" => "#FCF1EE",
+        "body_color" => "#FCF1E1",
+        "title_color" => "#333",
+        "body" => "Some body text to display"
+      })
+    end
+
+    it "should have a url" do
+      expect(@feed_item.url).to eql("https://www.example.com/a_page_to_open_on_tap.html")
+    end
+  end
+
+  context ".create" do
+    it "has specs" do
+      skip "implement"
+    end
   end
 end
