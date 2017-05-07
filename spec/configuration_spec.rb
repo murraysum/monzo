b5d9d3c289a42cf5c9527d191b55ac7cbcd62771
Murray Summers <murray.sum@gmail.com>
2017-05-07 20:18:23 +0100

    Add spec to ensure that the access token is set on the configuration.

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
 
 describe Monzo::Configuration do
-  it "has specs" do
-    skip "implement"
+  before :each do
+    @access_token = "abc"
+    @configuration = Monzo::Configuration.new(@access_token)
+  end
+
+  it "should have configured an access token" do
+    expect(@configuration.access_token).to eql(@access_token)
   end
 end
