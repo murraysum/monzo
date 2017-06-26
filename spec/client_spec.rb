a52f83d283dbcf8a4157bbfb11622e804008c9ab
Murray Summers <murray.sum@gmail.com>
2017-06-26 19:22:09 +0100

    Add client specs.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,19 +13,112 @@ describe Monzo::Client do
     end
   end
 
-  it "#get" do
-    skip "implement"
+
+  context "#get" do
+    before :each do
+      access_token = "abc"
+
+      @body = {test: "body"}.to_json
+      @stub = stub_request(:get, "https://api.monzo.com/test/?foo=bar").
+        with(headers: build_request_headers(access_token)).
+        to_return(status: 200, body: @body , headers: {})
+
+      client = Monzo::Client.new(access_token)
+
+      @response = client.get("/test/", :foo => "bar")
+    end
+
+    it "has performed the request" do
+      expect(@stub).to have_been_requested
+    end
+
+    it "should have a response code" do
+      expect(@response.code).to eql("200")
+    end
+
+    it "should have a response body" do
+      expect(@response.body).to eql(@body)
+    end
   end
 
-  it "#post" do
-    skip "implement"
+  context "#post" do
+    before :each do
+      access_token = "abc"
+
+      @body = {test: "body"}.to_json
+      @stub = stub_request(:post, "https://api.monzo.com/test/").
+        with(headers: build_request_headers(access_token)).
+        to_return(status: 200, body: @body , headers: {})
+
+      client = Monzo::Client.new(access_token)
+
+      @response = client.post("/test/", :foo => "bar")
+    end
+
+    it "has performed the request" do
+      expect(@stub).to have_been_requested
+    end
+
+    it "should have a response code" do
+      expect(@response.code).to eql("200")
+    end
+
+    it "should have a response body" do
+      expect(@response.body).to eql(@body)
+    end
   end
 
-  it "#patch" do
-    skip "implement"
+  context "#patch" do
+    before :each do
+      access_token = "abc"
+
+      @body = {test: "body"}.to_json
+      @stub = stub_request(:patch, "https://api.monzo.com/test/").
+        with(headers: build_request_headers(access_token)).
+        to_return(status: 200, body: @body , headers: {})
+
+      client = Monzo::Client.new(access_token)
+
+      @response = client.patch("/test/", :foo => "bar")
+    end
+
+    it "has performed the request" do
+      expect(@stub).to have_been_requested
+    end
+
+    it "should have a response code" do
+      expect(@response.code).to eql("200")
+    end
+
+    it "should have a response body" do
+      expect(@response.body).to eql(@body)
+    end
   end
 
-  it "#delete" do
-    skip "implement"
+  context "#delete" do
+    before :each do
+      access_token = "abc"
+
+      @body = {test: "body"}.to_json
+      @stub = stub_request(:delete, "https://api.monzo.com/test/").
+        with(headers: build_request_headers(access_token)).
+        to_return(status: 200, body: @body , headers: {})
+
+      client = Monzo::Client.new(access_token)
+
+      @response = client.delete("/test/", :foo => "bar")
+    end
+
+    it "has performed the request" do
+      expect(@stub).to have_been_requested
+    end
+
+    it "should have a response code" do
+      expect(@response.code).to eql("200")
+    end
+
+    it "should have a response body" do
+      expect(@response.body).to eql(@body)
+    end
   end
 end
